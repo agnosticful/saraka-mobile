a29d0c0d5e1ff9d5c8d55bc853f196d955ff5424
Kohei Asai <me@axross.io>
2019-03-03 14:27:48 -0800

    Add the button to show a dialog

--- lib/src/routes/signed_in/home_route/home_screen.dart
+++ lib/src/routes/signed_in/home_route/home_screen.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:provider/provider.dart';
 import 'package:saraka/constants.dart';
 import 'package:saraka/domains.dart';
+import './new_card_floating_action_button.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Container(
-        padding: EdgeInsets.symmetric(vertical: 64, horizontal: 32),
-        decoration: BoxDecoration(
-          color: SarakaColors.white,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            RaisedButton(
-              onPressed: () {
-                Provider.of<Authentication>(context).signOut();
-              },
-              child: Text('aaaa'),
-            ),
-          ],
+  Widget build(BuildContext context) => Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: NewCardFloatingActionButton(),
+        body: Container(
+          padding: EdgeInsets.symmetric(vertical: 64, horizontal: 32),
+          decoration: BoxDecoration(
+            color: SarakaColors.white,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              RaisedButton(
+                onPressed: () {
+                  Provider.of<Authentication>(context).signOut();
+                },
+                child: Text('aaaa'),
+              ),
+            ],
+          ),
         ),
       );
 }
