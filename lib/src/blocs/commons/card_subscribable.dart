2b8d93ae3eb597881828e3260701eb554dca1eaa
Kohei Asai <me@axross.io>
2019-03-17 20:22:01 -0700

    Make CardSubscribable mixin not common

--- lib/src/blocs/commons/card_subscribable.dart
+++
@@ -1,9 +0,0 @@
-import 'package:meta/meta.dart';
-import 'package:rxdart/rxdart.dart';
-import 'package:saraka/entities.dart';
-
-export 'package:saraka/entities.dart' show Card, User;
-
-mixin CardSubscribable {
-  Observable<Iterable<Card>> subscribeCards({@required User user});
-}
