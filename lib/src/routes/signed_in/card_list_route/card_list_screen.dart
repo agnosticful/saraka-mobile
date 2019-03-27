79dab93bf312e21392e329005a268c04ae21eec7
Kohei Asai <me@axross.io>
2019-03-26 17:23:20 -0700

    Arrange widgets in the screen’s one

--- lib/src/routes/signed_in/card_list_route/card_list_screen.dart
+++ lib/src/routes/signed_in/card_list_route/card_list_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:saraka/constants.dart';
-import 'package:wave/config.dart';
-import 'package:wave/wave.dart';
-import './card_list_view.dart';
+import './card_sliver_list.dart';
 import './new_card_floating_action_button.dart';
+import './wave_background.dart';
 
 class CardListScreen extends StatelessWidget {
   @override
@@ -13,28 +13,33 @@ class CardListScreen extends StatelessWidget {
         body: Container(
           child: Stack(
             children: [
-              WaveWidget(
-                config: CustomConfig(
-                  gradients: [
-                    [
-                      SarakaColors.lightGray.withOpacity(0.125),
-                      SarakaColors.darkGray.withOpacity(0.125)
-                    ],
-                    [
-                      SarakaColors.lightGray.withOpacity(0.25),
-                      SarakaColors.darkGray.withOpacity(0.25)
-                    ],
-                  ],
-                  durations: [19440, 6000],
-                  heightPercentages: [0.2, 0.5],
-                  gradientBegin: Alignment.bottomLeft,
-                  gradientEnd: Alignment.topRight,
-                ),
-                backgroundColor: SarakaColors.white,
-                size: Size.infinite,
-                waveAmplitude: 0,
+              WaveBackground(),
+              CustomScrollView(
+                slivers: [
+                  SliverAppBar(
+                    backgroundColor: Color(0x00000000),
+                    iconTheme: IconThemeData(color: SarakaColors.lightBlack),
+                    centerTitle: true,
+                    title: Text(
+                      'Cards',
+                      style: TextStyle(
+                        color: SarakaColors.lightBlack,
+                        fontFamily: SarakaFonts.rubik,
+                      ),
+                    ),
+                    leading: Navigator.of(context).canPop()
+                        ? IconButton(
+                            icon: Icon(Feather.getIconData('arrow-left')),
+                            onPressed: () => Navigator.of(context).pop(),
+                          )
+                        : null,
+                  ),
+                  SliverPadding(
+                    padding: EdgeInsets.fromLTRB(16, 16, 16, 80),
+                    sliver: CardSliverList(),
+                  ),
+                ],
               ),
-              CardListView(),
             ],
           ),
         ),
