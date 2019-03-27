0cb20d7077377cbffd0f82658396d5e21bc7f784
Kohei Asai <me@axross.io>
2019-03-27 14:24:30 -0700

    Click menu icon button to open CardDeleteConfirmDialog temporarily

--- lib/src/routes/signed_in/card_list_route/card_list_view_item/menu_icon_button.dart
+++ lib/src/routes/signed_in/card_list_route/card_list_view_item/menu_icon_button.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart' show IconButton;
 import 'package:flutter/widgets.dart';
 import 'package:flutter_icons/flutter_icons.dart';
+import 'package:saraka/blocs.dart';
 import 'package:saraka/constants.dart';
+import '../../card_delete_confirm_dialog/show_card_delete_confirm_dialog.dart';
 
 class MenuIconButton extends StatelessWidget {
+  MenuIconButton({Key key, @required Card card})
+      : assert(card != null),
+        _card = card,
+        super(key: key);
+
+  final Card _card;
+
   @override
   Widget build(BuildContext context) {
     return IconButton(
@@ -12,7 +21,7 @@ class MenuIconButton extends StatelessWidget {
         color: SarakaColors.darkGray,
       ),
       onPressed: () {
-        // TODO: implement "delete card" here
+        showCardDeleteConfirmDialog(context, card: _card);
       },
     );
   }
