3011a8873640776cdbeba18a99bb831e4d811733
Kohei Asai <me@axross.io>
2019-03-26 17:18:25 -0700

    Split out CardSliverList widget

--- lib/src/routes/signed_in/card_list_route/card_list_view.dart
+++ lib/src/routes/signed_in/card_list_route/card_list_view.dart
@@ -1,100 +1,39 @@
-import 'dart:math' show max;
 import 'package:flutter/material.dart' show IconButton, SliverAppBar;
 import 'package:flutter/widgets.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:provider/provider.dart';
 import 'package:saraka/blocs.dart';
 import 'package:saraka/constants.dart';
-import './card_list_view_item/card_list_view_item.dart';
+import './card_sliver_list.dart';
 
-class CardListView extends StatefulWidget {
-  State<CardListView> createState() => _CardListViewState();
-}
-
-class _CardListViewState extends State<CardListView> {
-  CardListBloc _cardListBloc;
-
-  CardDetailBlocFactory _cardDetailBlocFactory;
-
-  SynthesizerBlocFactory _synthesizerBlocFactory;
-
+class CardListView extends StatelessWidget {
   @override
-  void initState() {
-    super.initState();
-
-    Future.delayed(Duration.zero, () async {
-      final cardListBloc = Provider.of<CardListBloc>(context);
-      final cardDetailBlocFactory = Provider.of<CardDetailBlocFactory>(context);
-      final synthesizerBlocFactory =
-          Provider.of<SynthesizerBlocFactory>(context);
-
-      setState(() {
-        _cardListBloc = cardListBloc;
-        _cardDetailBlocFactory = cardDetailBlocFactory;
-        _synthesizerBlocFactory = synthesizerBlocFactory;
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) => _cardListBloc == null
-      ? Container()
-      : StreamBuilder<List<Card>>(
-          stream: _cardListBloc.cards.map((iter) => iter.toList()),
-          initialData: [],
-          builder: (context, snapshot) => CustomScrollView(
-                slivers: [
-                  SliverAppBar(
-                    backgroundColor: Color(0x00000000),
-                    iconTheme: IconThemeData(color: SarakaColors.lightBlack),
-                    centerTitle: true,
-                    title: Text(
-                      'Cards',
-                      style: TextStyle(
-                        color: SarakaColors.lightBlack,
-                        fontFamily: SarakaFonts.rubik,
-                      ),
-                    ),
-                    leading: Navigator.of(context).canPop()
-                        ? IconButton(
-                            icon: Icon(Feather.getIconData('arrow-left')),
-                            onPressed: () => Navigator.of(context).pop(),
-                          )
-                        : null,
-                  ),
-                  SliverPadding(
-                    padding: EdgeInsets.fromLTRB(16, 16, 16, 80),
-                    sliver: SliverList(
-                      delegate: SliverChildBuilderDelegate(
-                        (context, i) {
-                          final card = snapshot.requireData[i ~/ 2];
-
-                          return i.isEven
-                              ? MultiProvider(
-                                  key: ValueKey(card.id),
-                                  providers: [
-                                    StatefulProvider<CardDetailBloc>(
-                                      valueBuilder: (context) =>
-                                          _cardDetailBlocFactory.create(card),
-                                    ),
-                                    StatefulProvider<SynthesizerBloc>(
-                                      valueBuilder: (context) =>
-                                          _synthesizerBlocFactory.create(),
-                                    ),
-                                  ],
-                                  child: CardListViewItem(
-                                    card: card,
-                                  ),
-                                )
-                              : SizedBox(height: 16);
-                        },
-                        childCount: max(0, snapshot.requireData.length * 2 - 1),
-                      ),
-                    ),
-                  ),
-                ],
+  Widget build(BuildContext context) => CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Color(0x00000000),
+            iconTheme: IconThemeData(color: SarakaColors.lightBlack),
+            centerTitle: true,
+            title: Text(
+              'Cards',
+              style: TextStyle(
+                color: SarakaColors.lightBlack,
+                fontFamily: SarakaFonts.rubik,
               ),
-        );
+            ),
+            leading: Navigator.of(context).canPop()
+                ? IconButton(
+                    icon: Icon(Feather.getIconData('arrow-left')),
+                    onPressed: () => Navigator.of(context).pop(),
+                  )
+                : null,
+          ),
+          SliverPadding(
+            padding: EdgeInsets.fromLTRB(16, 16, 16, 80),
+            sliver: CardSliverList(),
+          ),
+        ],
+      );
 
   void _onSignOutPressed(BuildContext context) {
     Provider.of<AuthenticationBloc>(context).signOut();
---
+++ lib/src/routes/signed_in/card_list_route/card_sliver_list.dart
@@ -0,0 +1,51 @@
+import 'dart:math' as math;
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:saraka/blocs.dart';
+import './card_list_view_item/card_list_view_item.dart';
+
+class CardSliverList extends StatefulWidget {
+  @override
+  _CardSliverListState createState() => _CardSliverListState();
+}
+
+class _CardSliverListState extends State<CardSliverList> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer3<CardListBloc, CardDetailBlocFactory,
+        SynthesizerBlocFactory>(
+      builder: (context, cardListBloc, cardDetailBlocFactory,
+              synthesizerBlocFactory) =>
+          StreamBuilder<List<Card>>(
+            stream: cardListBloc.cards.map((iter) => iter.toList()),
+            initialData: [],
+            builder: (context, snapshot) => SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, i) {
+                      final card = snapshot.requireData[i ~/ 2];
+
+                      return i.isEven
+                          ? MultiProvider(
+                              key: ValueKey(card.id),
+                              providers: [
+                                Provider<CardDetailBloc>(
+                                  value: cardDetailBlocFactory.create(card),
+                                ),
+                                Provider<SynthesizerBloc>(
+                                  value: synthesizerBlocFactory.create(),
+                                ),
+                              ],
+                              child: CardListViewItem(
+                                card: card,
+                              ),
+                            )
+                          : SizedBox(height: 16);
+                    },
+                    childCount:
+                        math.max(0, snapshot.requireData.length * 2 - 1),
+                  ),
+                ),
+          ),
+    );
+  }
+}
