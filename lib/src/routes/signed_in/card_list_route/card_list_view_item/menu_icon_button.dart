c35277f76f4238ddf3a63e1560a759d04ed869aa
Kohei Asai <me@axross.io>
2019-03-27 17:42:25 -0700

    Popup menu

--- lib/src/routes/signed_in/card_list_route/card_list_view_item/menu_icon_button.dart
+++ lib/src/routes/signed_in/card_list_route/card_list_view_item/menu_icon_button.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart' show PopupMenuItem, showMenu;
 import 'package:flutter/material.dart' show IconButton;
 import 'package:flutter/widgets.dart';
 import 'package:flutter_icons/flutter_icons.dart';
@@ -20,9 +21,41 @@ class MenuIconButton extends StatelessWidget {
         Feather.getIconData('more-vertical'),
         color: SarakaColors.darkGray,
       ),
-      onPressed: () {
-        showCardDeleteConfirmDialog(context, card: _card);
-      },
+      onPressed: () => onPressed(context),
     );
+  }
+
+  void onPressed(BuildContext context) async {
+    final RenderBox self = context.findRenderObject();
+    final RenderBox overlay = Overlay.of(context).context.findRenderObject();
+
+    final selectedItem = await showMenu(
+      context: context,
+      position: RelativeRect.fromRect(
+        Rect.fromPoints(
+          self.localToGlobal(Offset.zero, ancestor: overlay),
+          self.localToGlobal(self.size.bottomRight(Offset.zero),
+              ancestor: overlay),
+        ),
+        Offset.zero & overlay.size,
+      ),
+      items: [
+        const PopupMenuItem(
+          value: "",
+          child: Text(
+            "Delete",
+            style: TextStyle(
+              color: SarakaColors.lightRed,
+              fontFamily: SarakaFonts.rubik,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ],
+    );
+
+    if (selectedItem != null) {
+      showCardDeleteConfirmDialog(context, card: _card);
+    }
   }
 }
