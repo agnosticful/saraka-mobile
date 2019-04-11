783f12cba4055d8539e2084f76ce305a35c98bb9
ryotogashi <ryo.togashi.ca@gmail.com>
2019-04-10 18:53:41 -0700

    edit cardlist screen’s appbar

--- lib/src/widgets/card_list_screen/card_list_screen.dart
+++ lib/src/widgets/card_list_screen/card_list_screen.dart
@@ -10,29 +10,30 @@ class CardListScreen extends StatelessWidget {
   Widget build(BuildContext context) => Scaffold(
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButton: NewCardFloatingActionButton(),
+        appBar: AppBar(
+          backgroundColor: SarakaColors.white,
+          elevation: 0,
+          iconTheme: IconThemeData(color: SarakaColors.lightBlack),
+          centerTitle: true,
+          title: Text(
+            'Cards',
+            style: SarakaTextStyles.appBarTitle.copyWith(
+              color: SarakaColors.lightBlack,
+            ),
+          ),
+          leading: Navigator.of(context).canPop()
+              ? IconButton(
+                  icon: Icon(Feather.getIconData('arrow-left')),
+                  onPressed: () => Navigator.of(context).pop(),
+                )
+              : null,
+        ),
         body: Container(
           child: Stack(
             children: [
               WaveBackground(),
               CustomScrollView(
                 slivers: [
-                  SliverAppBar(
-                    backgroundColor: Color(0x00000000),
-                    iconTheme: IconThemeData(color: SarakaColors.lightBlack),
-                    centerTitle: true,
-                    title: Text(
-                      'Cards',
-                      style: SarakaTextStyles.appBarTitle.copyWith(
-                        color: SarakaColors.lightBlack,
-                      ),
-                    ),
-                    leading: Navigator.of(context).canPop()
-                        ? IconButton(
-                            icon: Icon(Feather.getIconData('arrow-left')),
-                            onPressed: () => Navigator.of(context).pop(),
-                          )
-                        : null,
-                  ),
                   SliverPadding(
                     padding: EdgeInsets.fromLTRB(16, 16, 16, 80),
                     sliver: CardSliverList(),
