1a54abdc0ac0a4e5425cccc5459f29c538914f73
ryotogashi <ryo.togashi.ca@gmail.com>
2019-04-11 20:30:12 -0700

    Delete main drawer for moved main drawer to dashboard screen.

--- lib/src/widgets/study_screen/study_screen.dart
+++ lib/src/widgets/study_screen/study_screen.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart' show AppBar, IconButton, Scaffold;
 import 'package:flutter/widgets.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:saraka/constants.dart';
-import 'package:saraka/widgets.dart';
 import 'package:wave/config.dart';
 import 'package:wave/wave.dart';
 import './back_button.dart';
 import './card_bundle.dart';
 import './finished.dart';
-import './main_drawer.dart';
 import './progress_indicator.dart';
 import './time_estimation.dart';
 
