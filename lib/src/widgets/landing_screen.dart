d895fb5f8eb503e31011c71425fba75f3b2f503f
Kohei Asai <me@axross.io>
2019-04-12 20:17:21 -0700

    Change design of LandingScreen widget

--- lib/src/widgets/landing_screen.dart
+++ lib/src/widgets/landing_screen.dart
@@ -1,12 +1,41 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:saraka/constants.dart';
 
 class LandingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(color: Color(0xffffffff)),
-      child: const Center(
-        child: Text("Hello"),
+      decoration: BoxDecoration(
+        color: SarakaColors.white,
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 192,
+              height: 192,
+              child: Image.asset('assets/images/saraka.png'),
+            ),
+            SizedBox(height: 32),
+            Text(
+              "Saraka",
+              style: SarakaTextStyles.heading.copyWith(
+                fontSize: 32,
+                fontWeight: FontWeight.w300,
+                color: SarakaColors.lightRed,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              "Learn by practice.",
+              style: SarakaTextStyles.body.copyWith(
+                fontSize: 16,
+                color: Color(0x00ffffff),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
