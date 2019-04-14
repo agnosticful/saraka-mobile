5f0d901acd32d530beec9c4f5e8938b8eed47a99
Kohei Asai <me@axross.io>
2019-04-13 17:10:35 -0700

    Renew login screen (#102)
    * Change design of SignedOutScreen widget
    
    * Change design of SignInButton
    
    * Change design of LandingScreen widget

--- lib/src/widgets/signed_out_screen/sign_in_button.dart
+++ lib/src/widgets/signed_out_screen/sign_in_button.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:saraka/blocs.dart';
+import 'package:saraka/constants.dart';
+import 'package:saraka/widgets.dart';
 
-class SignInButton extends StatelessWidget {
+class SignInButton extends StatefulWidget {
   @override
-  Widget build(BuildContext context) => RaisedButton(
-        shape: StadiumBorder(),
-        padding: EdgeInsets.symmetric(
-          vertical: 16,
-          horizontal: 24,
-        ),
+  State<SignInButton> createState() => _SignInButtonState();
+}
+
+class _SignInButtonState extends State<SignInButton> {
+  bool _isProcessing = false;
+
+  @override
+  Widget build(BuildContext context) => ProcessableFancyButton(
+        color: SarakaColors.lightRed,
+        isProcessing: _isProcessing,
         onPressed: () => _onPressed(context),
-        child: Text('Sign in with Google'),
+        child: Text('Start with Google Account'),
       );
 
   void _onPressed(BuildContext context) {
+    setState(() {
+      _isProcessing = true;
+    });
+
     final authentication = Provider.of<AuthenticationBloc>(context);
 
     authentication.signIn();
--- lib/src/widgets/signed_out_screen/signed_out_screen.dart
+++ lib/src/widgets/signed_out_screen/signed_out_screen.dart
@@ -1,31 +1,52 @@
-import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/widgets.dart';
+import 'package:saraka/constants.dart';
 import './sign_in_button.dart';
 
 class SignedOutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Stack(
-        children: [
-          FlareActor(
-            "assets/flare/Mountain.flr",
-            alignment: Alignment.center,
-            fit: BoxFit.cover,
-            animation: "rotate",
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 128, horizontal: 64),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SignInButton(),
-              ],
+        decoration: BoxDecoration(
+          color: SarakaColors.white,
+        ),
+        child: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 192,
+                    height: 192,
+                    child: Image.asset('assets/images/saraka.png'),
+                  ),
+                  SizedBox(height: 32),
+                  Text(
+                    "Saraka",
+                    style: SarakaTextStyles.heading.copyWith(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w300,
+                      color: SarakaColors.lightRed,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "Learn by practice.",
+                    style: SarakaTextStyles.body.copyWith(
+                      fontSize: 16,
+                      color: SarakaColors.lightRed,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+            Positioned(
+              bottom: 128,
+              left: 64,
+              right: 64,
+              child: SignInButton(),
+            ),
+          ],
+        ));
   }
 }
