9ae56b85dd1d5846484334e75496caeecd68e3b1
ryotogashi <ryo.togashi.ca@gmail.com>
2019-04-29 00:01:20 -0700

    Create background.

--- lib/src/widgets/article_list_screen/article_list_screen.dart
+++ lib/src/widgets/article_list_screen/article_list_screen.dart
@@ -1,11 +1,30 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Card;
+import 'package:saraka/constants.dart';
+import 'package:saraka/widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:saraka/blocs.dart';
 
+// TODO has to change Consumer due to use CardListBloc
 class ArticleListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: Text("Hello Article screen"),
+        body: Container(
+          child: Stack(
+            children: <Widget>[
+              WaveBackground(color: SarakaColors.white),
+              Consumer3<CardListBloc, CardDetailBlocFactory,
+                  SynthesizerBlocFactory>(
+                builder: (context, cardListBloc, cardDetailBlocFactory,
+                        synthesizerBlocFactory) =>
+                    StreamBuilder<List<Card>>(
+                      stream: cardListBloc.cards,
+                      initialData: cardListBloc.cards.value,
+                      builder: (context, snapshot) =>
+                          snapshot.hasData ? Container() : Container(),
+                    ),
+              ),
+            ],
+          ),
         ),
-        body: Text("Hello article containts"),
       );
 }
