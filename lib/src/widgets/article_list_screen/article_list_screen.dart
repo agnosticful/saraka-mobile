29e55fba0cd146523fcdba1b5cfc9be09b17db2a
ryotogashi <ryo.togashi.ca@gmail.com>
2019-04-29 00:01:48 -0700

    comment code from card list screen.

--- lib/src/widgets/article_list_screen/article_list_screen.dart
+++ lib/src/widgets/article_list_screen/article_list_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart' hide Card;
 import 'package:saraka/constants.dart';
 import 'package:saraka/widgets.dart';
-import 'package:provider/provider.dart';
-import 'package:saraka/blocs.dart';
+// import 'package:provider/provider.dart';
+// import 'package:saraka/blocs.dart';
 
 // TODO has to change Consumer due to use CardListBloc
 class ArticleListScreen extends StatelessWidget {
@@ -12,17 +12,17 @@ class ArticleListScreen extends StatelessWidget {
           child: Stack(
             children: <Widget>[
               WaveBackground(color: SarakaColors.white),
-              Consumer3<CardListBloc, CardDetailBlocFactory,
-                  SynthesizerBlocFactory>(
-                builder: (context, cardListBloc, cardDetailBlocFactory,
-                        synthesizerBlocFactory) =>
-                    StreamBuilder<List<Card>>(
-                      stream: cardListBloc.cards,
-                      initialData: cardListBloc.cards.value,
-                      builder: (context, snapshot) =>
-                          snapshot.hasData ? Container() : Container(),
-                    ),
-              ),
+              // Consumer3<CardListBloc, CardDetailBlocFactory,
+              //     SynthesizerBlocFactory>(
+              //   builder: (context, cardListBloc, cardDetailBlocFactory,
+              //           synthesizerBlocFactory) =>
+              //       StreamBuilder<List<Card>>(
+              //         stream: cardListBloc.cards,
+              //         initialData: cardListBloc.cards.value,
+              //         builder: (context, snapshot) =>
+              //             snapshot.hasData ? Container() : Container(),
+              //       ),
+              // ),
             ],
           ),
         ),
