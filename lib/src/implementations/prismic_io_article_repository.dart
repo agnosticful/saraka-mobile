654d2cadece4a23dbeca48aa1ab1842620328128
ryotogashi <ryo.togashi.ca@gmail.com>
2019-04-30 22:52:36 -0700

    add http get json.

--- lib/src/implementations/prismic_io_article_repository.dart
+++ lib/src/implementations/prismic_io_article_repository.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:rxdart/rxdart.dart';
 import '../blocs/article_gettable.dart';
 import './prismic_io_article.dart';
@@ -11,14 +12,16 @@ class PrismicIoArticleRepository implements ArticleGettable {
     const url =
         "https://saraka.cdn.prismic.io/api/v2/documents/search?ref=XMfh-RAAABUjzjv8";
 
-    Map<String, dynamic> jsonData = jsonDecode(url);
+    http.get(url).then((resopnce) {
+      Map<String, dynamic> jsonData = jsonDecode(resopnce.body);
 
-    if (jsonData['result'].hasData) {
-      final articles = jsonData['result']
-          .map((article) => PrismicIoArticle(article['data']));
+      if (jsonData['result'].hasData) {
+        final articles = jsonData['result']
+            .map((article) => PrismicIoArticle(article['data']));
 
-      observable.add(articles);
-    }
+        observable.add(articles);
+      }
+    });
 
     return observable;
   }
