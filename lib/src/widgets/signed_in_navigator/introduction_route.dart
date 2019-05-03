6f6ccec829fd2d435de3a7d63b2e5ecdd91171d8
Ryo Togashi <35892492+ryotogashi@users.noreply.github.com>
2019-05-03 16:17:36 -0700

    add session argument to introduction route (#195)
    * add session argument to introduction route
    
    * delete unneccessary line and change factory
    
    * add item count.
    
    * change pageCount from variable.
    
    * Revert "add item count."
    
    This reverts commit c926e524d580be5347afff1728345e53ecb806e7.
    
    # Conflicts:
    #	lib/src/widgets/introduction_slider/introduction_slider.dart
    
    * Revert "change pageCount from variable."
    
    This reverts commit 60d116efaddbac72a12998c7d9954a8ef9c6eb31.
    
    # Conflicts:
    #	lib/src/widgets/introduction_slider/introduction_slider.dart
    
    * revert commit

--- lib/src/widgets/signed_in_navigator/introduction_route.dart
+++ lib/src/widgets/signed_in_navigator/introduction_route.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
+import '../../blocs/authentication_bloc.dart';
 import '../../blocs/introduction_bloc.dart';
 
 class IntroductionRoute extends CupertinoPageRoute {
   IntroductionRoute({@required Widget child, RouteSettings settings})
       : super(
-          builder: (BuildContext context) => StatefulProvider(
-                valueBuilder: (_) =>
-                    Provider.of<IntroductionBlocFactory>(context).create(),
-                child: child,
+          builder: (BuildContext context) =>
+              Consumer2<AuthenticationBloc, IntroductionBlocFactory>(
+                builder:
+                    (context, authenticationBloc, introductionBlocFactory) =>
+                        StatefulProvider(
+                          valueBuilder: (_) => introductionBlocFactory.create(
+                                session: authenticationBloc.session,
+                              ),
+                          child: child,
+                        ),
               ),
           settings: settings,
         );
