9e9557659c0c680271754d651c5e84a69f163236
ryotogashi <ryo.togashi.ca@gmail.com>
2019-05-12 01:14:28 -0700

    show intro when dashboard is open.

--- lib/src/widgets/dashboard_screen/dashboard_screen.dart
+++ lib/src/widgets/dashboard_screen/dashboard_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart' hide AppBar, Card;
 import 'package:saraka/constants.dart';
 import '../wave_background.dart';
@@ -8,52 +9,60 @@ import './ready_card_length_text.dart';
 import './start_learning_floating_action_button.dart';
 import './summary.dart';
 
+import '../midway_introduction_screen.dart';
+
 class DashboardScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Stack(
-        children: [
-          WaveBackground(color: SarakaColors.white),
-          SafeArea(
-            child: Scaffold(
-              resizeToAvoidBottomPadding: false,
-              backgroundColor: Color(0x00000000),
-              floatingActionButtonLocation:
-                  FloatingActionButtonLocation.centerFloat,
-              floatingActionButton: StartLearningFloatingActionButton(),
-              appBar: AppBar(),
-              drawer: MainDrawer(),
-              body: Column(
-                children: [
-                  Expanded(
-                    child: LayoutBuilder(
-                      builder: (context, constraints) => Center(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                ConstrainedBox(
-                                  constraints: BoxConstraints(
-                                    minWidth: 280,
-                                    minHeight: 280,
-                                    maxWidth: 280,
-                                    maxHeight: 280,
-                                  ),
-                                  child: Summary(),
+  Widget build(BuildContext context) {
+    Future.delayed(
+      Duration.zero,
+      () => showMidwayIntroductionDialog(context: context),
+    );
+    return Stack(
+      children: [
+        WaveBackground(color: SarakaColors.white),
+        SafeArea(
+          child: Scaffold(
+            resizeToAvoidBottomPadding: false,
+            backgroundColor: Color(0x00000000),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+            floatingActionButton: StartLearningFloatingActionButton(),
+            appBar: AppBar(),
+            drawer: MainDrawer(),
+            body: Column(
+              children: [
+                Expanded(
+                  child: LayoutBuilder(
+                    builder: (context, constraints) => Center(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              ConstrainedBox(
+                                constraints: BoxConstraints(
+                                  minWidth: 280,
+                                  minHeight: 280,
+                                  maxWidth: 280,
+                                  maxHeight: 280,
                                 ),
-                                SizedBox(height: 16),
-                                ProficientCardPredictionText(),
-                              ],
-                            ),
+                                child: Summary(),
+                              ),
+                              SizedBox(height: 16),
+                              ProficientCardPredictionText(),
+                            ],
                           ),
-                    ),
+                        ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 80),
-                    child: ReadyCardLengthText(),
-                  ),
-                ],
-              ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 80),
+                  child: ReadyCardLengthText(),
+                ),
+              ],
             ),
           ),
-        ],
-      );
+        ),
+      ],
+    );
+  }
 }
